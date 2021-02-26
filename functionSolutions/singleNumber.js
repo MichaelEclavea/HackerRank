1642b26dc29271f759601be7ff1159c79c15786c
MichaelEclavea <>
2021-02-25 22:15:36 -0800

    single number and reverse string

---
+++ functionSolutions/singleNumber.js
@@ -0,0 +1,15 @@
+var singleNumber = function(nums) {
+    let obj = {};
+    for(let i = 0; i< nums.length; i++){
+        if(!obj[nums[i]]){
+            obj[nums[i]] = 1; 
+        } else{
+            obj[nums[i]] += 1; 
+        }
+    }
+    for(const num in obj){
+        if(obj[num] === 1){
+            return num;
+        }
+    }
+};
